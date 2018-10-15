221a2e064d84508fdce43ce9de163e49485b3ce4
Diana de la Cruz <dianadlc310701@gmail.com>
2018-10-15 08:44:35 -0500

    MisDispositivo subir

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,36 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { BocinaPage } from '../bocina/bocina';
+import { TvPage } from '../tv/tv';
+import { SmartPage } from '../smart/smart';
+import { LamparaPage } from '../lampara/lampara';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  
+  bocina = BocinaPage;
+  tv = TvPage;
+  smart = SmartPage;
+  lampara = LamparaPage;
 
   constructor(public navCtrl: NavController) {
 
+  }
+
+  clickBoc(){
+    this.navCtrl.push(this.bocina)
+  }
+  clickTV(){
+    this.navCtrl.push(this.tv)
+  }
+  clickSmart(){
+    this.navCtrl.push(this.smart)
+  }
+  clickLam(){
+    this.navCtrl.push(this.lampara)
   }
 
 }
